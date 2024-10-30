c12fb39a7797ced4793bff560cb199e91368c50e
androiderNN <kento.i.1103@gmail.com>
2024-10-30 19:14:18 +0900

    initial commit

---
+++ src/config.py
@@ -0,0 +1,8 @@
+import os
+
+home_dir = os.path.dirname(os.getcwd())
+src_dir = os.path.join(home_dir, 'src')
+
+data_dir = os.path.join(home_dir, 'data')
+raw_dir = os.path.join(data_dir, 'raws')
+df_dir = os.path.join(data_dir, 'df')
