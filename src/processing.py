1be92c107243f7f14792948aa273bd60bdf403ed
androiderNN <kento.i.1103@gmail.com>
2024-10-31 17:58:40 +0900

    各関数の実装変更、築年数の作成

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-import os, pickle
+import os, pickle, datetime, math
 import numpy as np
 import pandas as pd
 
@@ -41,12 +41,58 @@ def target_encoding(train:pd.DataFrame, test, columns):
 
     return train, test
 
+def replace_values(df:pd.DataFrame) -> None:
+    # floor_count
+    col = 'floor_count'
+    df.loc[(df[col]>65)|(df[col]==0), col] = None  # 異常値の削除
+    dic = {i+j: i for i in range(16, 65, 5) for j in range(5)}
+    df[col] = df[col].replace(dic)
+
+    # building_structure
+    col = 'building_structure'
+    dic = {2:None, 7:None, 12:None, 13:None}    # データ数の少ないクラスをnanに
+    df[col] = df[col].replace(dic).astype(float)
+
+    # year_built
+    col = 'year_built'
+    df.loc[df[col]<1900, col] = None
+    df.loc[(df[col]<1950)&(df['floor_count']>=3), col] = None
+    # yb = np.array([s[:4]+'-'+s[4:6] if len(s)==8 else '' for s in df['year_built'].astype(str)], dtype=np.datetime64)   # length=3ならNaN、8なら年月
+    # bcd = np.array(df['building_create_date'], dtype=np.datetime64)
+    # diff = bcd - yb # 築年月とデータ登録日時の差
+    # df.loc[diff<0, 'year_built'] = None # 築年月がデータ登録以前の欄はnanに
+
+def create_new_cols(df:pd.DataFrame) -> None:
+    # old_months
+    yb = np.array([s[:4]+'-'+s[4:6] if len(s)==8 else '' for s in df['year_built'].astype(str)], dtype=np.datetime64)   # length=3ならNaN、8なら年月
+    bmd = np.array(df['building_modify_date'], dtype=np.datetime64)
+    old_year = (bmd-yb).astype('timedelta64[Y]')
+    df['old_year'] = pd.Series(old_year, dtype=float)
+
+def drop_cols(df:pd.DataFrame):
+    cols = [
+        'year_built'    # old_yearで築年数を表したため不要と思われる
+    ]
+    return df.drop(columns=cols)
+
 def process():
     train_df = pickle.load(open(config.tmp_train_df, 'rb'))
     test_df = pickle.load(open(config.tmp_test_df, 'rb'))
 
     te_cols = ['madori_kind_all']
     train_df, test_df = target_encoding(train_df, test_df, te_cols)
+
+    # 置換
+    replace_values(train_df)
+    replace_values(test_df)
+
+    # 特徴量作成
+    create_new_cols(train_df)
+    create_new_cols(test_df)
+
+    # 列削除
+    train_df = drop_cols(train_df)
+    test_df = drop_cols(test_df)
 
     # dtypeで抽出
     train_df = train_df.select_dtypes(include='number')
